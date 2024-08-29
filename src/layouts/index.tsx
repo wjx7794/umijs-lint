0e7285e84ae82da2b3a97e49ef0721e10758569b
wjx7794 <wjx7794@163.com>
2024-08-29 22:37:01 +0800

    1

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,11 +1,12 @@
-import { Link, Outlet } from 'umi';
-import styles from './index.less';
+import { Link, Outlet } from "umi";
+import styles from "./index.less";
 
 export default function Layout() {
   return (
     <div className={styles.navs}>
       <ul>
         <li>
+          
           <Link to="/">Home</Link>
         </li>
         <li>
